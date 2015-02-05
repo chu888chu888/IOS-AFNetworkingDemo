c8ec3d2726bdcd65d30b9aeca45f2465885f7309
chuguangming <chu888chu888@qq.com>
2015-02-05 17:49:08 +0800

    解决JSON返回值问题

--- AFDemo/AFDemo/AFViewController.m
+++ AFDemo/AFDemo/AFViewController.m
@@ -7,14 +7,15 @@
 //
 
 #import "AFViewController.h"
-#import "AFHTTPRequestOperationManager.h"
-#import "AFURLSessionManager.h"
-@interface AFViewController ()
 
+#import "AFNetworking.h"
+#import "HttpReturnModel.h"
+@interface AFViewController ()
+@property(strong,nonatomic)NSString *returnJsonStr;
 @end
 
 @implementation AFViewController
-
+@synthesize returnJsonStr;
 - (void)AFHTTPRequestOperationManagerDemo {
     //封装与Web应用程序通过HTTP交互，包括请求的创建，响应序列化，
     //网络可达性监控和安全，以及请求操作管理的常见沟通模式。
@@ -109,11 +110,77 @@
     //[self AFHTTPRequestOperationManagerDemo];
     //[self AFURLSessionManagerDownloadDemo];
     //[self AFURLSessionManagerUploadDemo];
-    
     //[self AFURLSessionManagerUploadDemo2];
     
     
+    AFHTTPRequestOperationManager *operationManager = [AFHTTPRequestOperationManager manager];
+    NSDictionary *params = @{@"username": @"13145877854",@"client_id":@"ObpJAwJ7WP4s4Rwd",
+                                 @"client_secret":@"WMv9vbYIFz8ugpwl6zDNThzn4KLoxLTV",
+                                 @"grant_type":@"password",@"password":@"888888"};
     
+    operationManager.requestSerializer = [AFHTTPRequestSerializer serializer];
+    
+    [operationManager.requestSerializer setValue:@"application/vnd.591m.v1+json" forHTTPHeaderField:@"Accept"];
+    [operationManager.requestSerializer setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
+
+    operationManager.responseSerializer = [AFJSONResponseSerializer serializer];
+    operationManager.responseSerializer.acceptableContentTypes = [NSSet setWithObject:@"application/json"];
+    
+
+
+    
+    
+    [operationManager POST:@"http://121.42.51.177/auth/access-token"
+                parameters:params
+                   success:^(AFHTTPRequestOperation *operation, id responseObject) {
+                       
+                       
+                       NSString *value1 = responseObject[@"access_token"];
+                       NSString *value2 = responseObject[@"expires_in"];
+                       NSString *value3 = responseObject[@"refresh_token"];
+                       NSString *value4 = responseObject[@"token_type"];
+
+                       /*
+                       NSLog(@"--------------------------");
+                       NSString* json = @"{\"access_token\":\"access_token\", \"expires_in\":\"access_token\",\"refresh_token\":\"access_token\",\"token_type\":\"access_token\",}";
+                       HttpReturnModel *returnModel=[[HttpReturnModel alloc]initWithString:returnJsonStr error:nil];
+                       NSLog(@"Model:%@",returnModel.access_token);
+                        */
+                       NSLog(@"");
+                       
+                   }
+                   failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+                       
+                       NSLog(@"Error: %@", [error description]);
+                       
+                   }
+     ];
+   
+    /*
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    
+    NSDictionary *dict1 = @{@"username": @"13145877854"};
+    NSDictionary *dict2 = @{@"client_id": @"ObpJAwJ7WP4s4Rwd"};
+    NSDictionary *dict3 = @{@"client_secret": @"WMv9vbYIFz8ugpwl6zDNThzn4KLoxLTV"};
+    NSDictionary *dict4 = @{@"grant_type": @"password"};
+    NSDictionary *dict5 = @{@"password": @"888888"};
+    NSArray *array = @[dict1, dict2, dict3, dict4, dict5];
+    // 设置请求格式
+    //manager.requestSerializer = [AFJSONRequestSerializer serializer];
+    
+    [manager.requestSerializer setValue:@"application/vnd.591m.v1+json" forHTTPHeaderField:@"Accept"];
+    [manager.requestSerializer setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
+    // 设置返回格式
+    //manager.responseSerializer = [AFHTTPResponseSerializer serializer];
+    
+    [manager POST:@"http://121.42.51.177/auth/access-token" parameters:array success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSString *result = [[NSString alloc] initWithData:responseObject encoding:NSUTF8StringEncoding];
+        
+        NSLog(@"%@", result);
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"%@",error);
+    }];
+     */
     
 }
 
